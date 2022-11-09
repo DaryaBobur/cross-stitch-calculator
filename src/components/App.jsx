6631cc90e89df226a79db585165864295ad91cca
Darya <daryabobur@gmail.com>
2022-11-09 22:18:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,40 @@
 import Form from "./Form/Form";
-
+import { Toaster } from 'react-hot-toast'
+// import { useState } from "react";
 
 const App = () => {
+  // const addContact = (data) => {
+  //   if(duplicateName(data)) {
+  //     return toast.error(`${data.name} is already in contacts!`)
+  //   }
+
+  //   const contact = {
+  //     id: nanoid(),
+  //     name: data.name,
+  //     number: data.number,
+  //   }
+   
+  //   setContacts(prevState => [contact, ...prevState]);
+  // };
+//   const [number, setNumber] = useState(0)
+
+// const readValue =(data)=> {
+//       const value = {
+//       // id: nanoid(),
+//       numbers: data.number,
+//       // height: data.height,
+//       // margin: data.margin,
+//     }
+//     setNumber(value)
+   
+// }
+
   return (
     <div>
       <h1>Cross Stitch Calculator</h1>
 
       <Form/>
+      <Toaster/>
     </div>
   );
 };
