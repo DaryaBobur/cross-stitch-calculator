7f8d0d03de313e29ecfcef6ad4bf9458baf2ee81
Darya <daryabobur@gmail.com>
2022-11-10 18:36:12 +0200

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,33 @@
+import Form from "../Form/Form";
+import { Toaster } from 'react-hot-toast'
+import { useState } from "react";
+import InfoEmbroidery from "../InfoEmbroidery";
+import { Title, Container } from "./AppStyled";
+
+
+const App = () => {
+
+  const [sizeWork, setSizeWork] = useState('')
+  const sizeEmbroidery = (data) => {
+    console.log(data)
+  const stitch = {
+    width: data.widthStitch,
+    height: data.heightStitch,
+    margin: data.marginStitch,
+    size: data.size,
+  }  
+  setSizeWork(stitch)
+  }
+
+  return (
+    <Container>
+      <Title>Cross Stitch Calculator</Title>
+
+      <Form onSubmit={sizeEmbroidery}/>
+      {sizeWork && <InfoEmbroidery size={sizeWork}/>}
+      <Toaster/>
+    </Container>
+  );
+};
+
+export default App;
