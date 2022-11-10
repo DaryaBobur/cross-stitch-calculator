8a6b2fdf7c3a86bae4dd6f70b07e3a913016a6d2
Darya <daryabobur@gmail.com>
2022-11-10 18:36:24 +0200

    Update InfoEmbroidery.jsx

--- src/components/InfoEmbroidery.jsx
+++ src/components/InfoEmbroidery.jsx
@@ -1,8 +1,8 @@
 
-const InfoEmbroidery = ({size}) => {
-    const sizeCanvaWidth = () => Math.ceil((size.width * 2.54 / size.size) + (size.margin * 2));
+const InfoEmbroidery = ({ size }) => {
+    const sizeCanvaWidth = () => ((size.width * 2.54 / size.size) + (size.margin * 2)).toFixed(2);
     
-    const sizeCanvaHeight = () => Math.ceil((size.height * 2.54 / size.size) + (size.margin * 2));
+    const sizeCanvaHeight = () => ((size.height * 2.54 / size.size) + (size.margin * 2)).toFixed(2);
     return (
         <div>
             <p>Розмір вишивки: {sizeCanvaWidth()} x {sizeCanvaHeight()} см.</p>
