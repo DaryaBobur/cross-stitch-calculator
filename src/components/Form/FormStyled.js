a825e484cd5185f3cc8734b22dfd0782a4f51485
Darya <daryabobur@gmail.com>
2022-11-11 16:51:58 +0200

    Update FormStyled.js

--- src/components/Form/FormStyled.js
+++ src/components/Form/FormStyled.js
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled';
 
 export const FormStyled = styled.form`
+
 display: flex;
 flex-direction: column;
 gap: 10px;
 
+p {
+    margin-right: 5px;
+}
+
 button {
     width: 250px;
+    font-weight: 700;
+    /* background-color: #ffeb3b;
+    color: #31b0e9; */
 }
 `
