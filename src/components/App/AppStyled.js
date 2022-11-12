804efac083b5a7a732acbc3c081a4c0f31cd9552
Darya <daryabobur@gmail.com>
2022-11-12 16:18:52 +0200

    Update AppStyled.js

--- src/components/App/AppStyled.js
+++ src/components/App/AppStyled.js
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+display: flex;
+flex-direction: column;
 padding: 30px;
 `
 
 export const Title = styled.h1`
 text-align: center;
+margin-bottom: 50px;
 `
