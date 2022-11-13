c375efac75b8bdc4f4f2020b157f5131d2eb0dfa
Darya <daryabobur@gmail.com>
2022-11-13 12:32:40 +0200

    Update FormStyled.js

--- src/components/Form/FormStyled.js
+++ src/components/Form/FormStyled.js
@@ -13,7 +13,6 @@ label {
     display: flex;
     align-items: center;
     gap: 10px;
-
     font-size: 22px;
     font-weight: 500;
 }
@@ -23,10 +22,13 @@ button {
     margin-right: auto;
     margin-left: auto;
     width: 250px;
-    font-size: 16px;
+    font-size: 22px;
     font-weight: 700;
-    margin-top: 15px;
-    /* background-color: #ffeb3b;
-    color: #31b0e9; */
+    margin-top: 30px;
+    margin-bottom: 30px;
+    background-size: contain;
+    color: #134c15;
+    -webkit-text-stroke: 1px black;
+    font-family: 'Philosopher', sans-serif;
 }
 `
