ac22b53922e56acce1231304c74517575ae72228
Darya <daryabobur@gmail.com>
2022-11-13 12:32:35 +0200

    Update AppStyled.js

--- src/components/App/AppStyled.js
+++ src/components/App/AppStyled.js
@@ -1,12 +1,29 @@
 import styled from '@emotion/styled';
 
+
 export const Container = styled.div`
 display: flex;
 flex-direction: column;
 padding: 30px;
+font-family: 'Philosopher', sans-serif;
+
 `
 
 export const Title = styled.h1`
+
 text-align: center;
 margin-bottom: 50px;
+font-family: 'Philosopher', sans-serif;
+font-weight: 700;
+`
+
+export const Wrap = styled.div`
+width: 630px;
+display: inline-flex;
+flex-direction: column;
+padding: 20px;
+border: 2px solid green;
+border-radius: 20px;
+margin-right: auto;
+margin-left: auto;
 `
