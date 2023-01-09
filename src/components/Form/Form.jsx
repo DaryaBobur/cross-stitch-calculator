bbf3d846b852a95d2a489129ffbe278d09c9374b
Darya <daryabobur@gmail.com>
2023-01-09 18:28:56 +0200

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,162 +1,165 @@
-import { FormStyled } from "./FormStyled";
-import { useState } from "react";
+import { FormStyled } from './FormStyled';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import NativeSelect from '@mui/material/NativeSelect';
-import img from '../../img/w3.jpg'
+import img from '../../img/w3.jpg';
 const Form = ({ onSubmit }) => {
-    
-    const [widthStitch, setWidthStitch] = useState('');
-    const [heightStitch, setHeightStitch] = useState('');
-    const [marginStitch, setMarginStitch] = useState('');
-    const [size, setSize] = useState('');
-    const [amountYarn, setAmountYarn] = useState('')
-  
-    const handleChange = evt => {
-        console.log(evt.target.value)
-        const { name, value } = evt.currentTarget;
+  const [widthStitch, setWidthStitch] = useState('');
+  const [heightStitch, setHeightStitch] = useState('');
+  const [marginStitch, setMarginStitch] = useState('');
+  const [size, setSize] = useState('');
+  const [amountYarn, setAmountYarn] = useState('');
 
-        switch(name) {
-          case 'widthStitch':
-          setWidthStitch(value);
-          break;
-  
-          case 'heightStitch':
-          setHeightStitch(value);
-          break;
-          
-          case 'marginStitch':
-          setMarginStitch(value);
-          break;
-  
-          case 'size':
-          setSize(value);
-          break;
+  const handleChange = evt => {
+    console.log(evt.target.value);
+    const { name, value } = evt.currentTarget;
 
-          case 'amountYarn':
-          setAmountYarn(value);
-          break;
+    switch (name) {
+      case 'widthStitch':
+        setWidthStitch(value);
+        break;
 
-          default:
-          return;
+      case 'heightStitch':
+        setHeightStitch(value);
+        break;
 
-          }
-      };
+      case 'marginStitch':
+        setMarginStitch(value);
+        break;
 
-  
-    const handleSubmit = e => {
-        e.preventDefault();
-        onSubmit({ widthStitch, heightStitch, marginStitch, size, amountYarn });
-        resetForm();
-    };
+      case 'size':
+        setSize(value);
+        break;
 
+      case 'amountYarn':
+        setAmountYarn(value);
+        break;
 
-  
-    const resetForm = () => {
-        setWidthStitch('');
-        setHeightStitch('');
-        setMarginStitch('');
-        setSize('');
+      default:
+        return;
     }
+  };
 
-    return (
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ widthStitch, heightStitch, marginStitch, size, amountYarn });
+    resetForm();
+  };
 
-        <FormStyled autocomplete="off" onSubmit={handleSubmit}>
-            
-            <Box sx={{display: 'flex', alignItems: 'center'}}>
-            <label>Розмір вишивки в хрестиках:
-            <TextField 
-            sx={{width: '80px', marginRight: '5px'}}
-            variant="outlined" 
-            type="text" 
-            name="widthStitch" 
-            onChange={handleChange} 
+  const resetForm = () => {
+    setWidthStitch('');
+    setHeightStitch('');
+    setMarginStitch('');
+    setSize('');
+    setAmountYarn('');
+  };
+
+  return (
+    <FormStyled autocomplete="off" onSubmit={handleSubmit}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <label>
+          Розмір вишивки в хрестиках:
+          <TextField
+            sx={{ width: '80px', marginRight: '5px' }}
+            variant="outlined"
+            type="text"
+            name="widthStitch"
+            onChange={handleChange}
             value={widthStitch}
             size="small"
             required
-            />
-            x
-            <TextField 
-            sx={{width: '80px', marginLeft: '5px'}}
-            variant="outlined" 
-            type="text" 
-            name="heightStitch" 
-            onChange={handleChange} 
+          />
+          x
+          <TextField
+            sx={{ width: '80px', marginLeft: '5px' }}
+            variant="outlined"
+            type="text"
+            name="heightStitch"
+            onChange={handleChange}
             value={heightStitch}
             size="small"
             required
-            />
-            </label>
-            </Box>
-            <Box sx={{display: 'flex', alignItems: 'center'}}>
-            <label>Відступ від краю(см):
-            <TextField 
-            type="text" 
-            sx={{width: '60px'}}
-            name="marginStitch" 
+          />
+        </label>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <label>
+          Відступ від краю(см):
+          <TextField
+            type="text"
+            sx={{ width: '60px' }}
+            name="marginStitch"
             onChange={handleChange}
             value={marginStitch}
             size="small"
             required
-            />
-            </label>
-            </Box>
-            <Box sx={{display: 'flex', alignItems: 'center'}}>
-            <label>Через скільки ниток будемо вишивати:
-            
-            <NativeSelect 
-            name="amountYarn" 
-            value={amountYarn} 
-            onChange={handleChange} 
-            size="small" 
-            sx={{width: '150px'}} 
-            required>
-                <option>Кількість ниток...</option>
-                <option value="1">1 нитка</option>
-                <option value="2">2 нитки</option>
-            </NativeSelect>
-            </label>
-            </Box>
+          />
+        </label>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <label>
+          Через скільки ниток будемо вишивати:
+          <NativeSelect
+            name="amountYarn"
+            value={amountYarn}
+            onChange={handleChange}
+            size="small"
+            sx={{ width: '150px' }}
+            required
+          >
+            <option>Кількість ниток...</option>
+              <option value="1">1 нитка</option>
+              <option value="2">2 нитки</option>
+          </NativeSelect>
+        </label>
+      </Box>
 
-            <Box sx={{display: 'flex', alignItems: 'center'}}></Box>
-            <label>Розмір вишивки на канві:
-            <NativeSelect 
-            name="size" 
-            value={size} 
-            onChange={handleChange} 
-            size="small" 
-            sx={{width: '150px'}} 
-            required>
-                <option>Оберіть канву...</option>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}></Box>
+      <label>
+        Розмір вишивки на канві:
+        <NativeSelect
+          name="size"
+          value={size}
+          onChange={handleChange}
+          size="small"
+          sx={{ width: '150px' }}
+          required
+        >
+          <option>Оберіть канву...</option>
 
-                <optgroup label="Канва">
-                    <option value="14">14 каунт</option>
-                    <option value="16">16 каунт</option>
-                    <option value="18">18 каунт</option>
-                    <option value="20">20 каунт</option>
-                    <option value="22">22 каунт</option>
-                </optgroup>
+          <optgroup label="Канва">
+            <option value="14">14 каунт</option>
+            <option value="16">16 каунт</option>
+            <option value="18">18 каунт</option>
+            <option value="20">20 каунт</option>
+            <option value="22">22 каунт</option>
+          </optgroup>
 
-                <optgroup label="Канва рівномірного плетіння">
-                    <option value="25">25 каунт</option>
-                    <option value="27">27 каунт</option>
-                    <option value="28">28 каунт</option>
-                    <option value="30">30 каунт</option>
-                    <option value="32">32 каунт</option>
-                    <option value="36">36 каунт</option>
-                    <option value="40">40 каунт</option>
-                    <option value="46">46 каунт</option>
-                    <option value="56">56 каунт</option>
-                </optgroup>
+          <optgroup label="Канва рівномірного плетіння">
+            <option value="25">25 каунт</option>
+            <option value="27">27 каунт</option>
+            <option value="28">28 каунт</option>
+            <option value="30">30 каунт</option>
+            <option value="32">32 каунт</option>
+            <option value="36">36 каунт</option>
+            <option value="40">40 каунт</option>
+            <option value="46">46 каунт</option>
+            <option value="56">56 каунт</option>
+          </optgroup>
+        </NativeSelect>
+      </label>
 
-            </NativeSelect>
-            </label>
-       
-            <Button type="submit" variant="contained" sx={{backgroundImage: `url(${img})`}}>Розрахунок</Button>
-        </FormStyled>
-
-    )
-}
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{ backgroundImage: `url(${img})` }}
+      >
+        Розрахунок
+      </Button>
+    </FormStyled>
+  );
+};
 
 export default Form;
