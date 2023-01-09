42fcc899561fdb8a331a6b5797e4ab353e23e008
Darya <daryabobur@gmail.com>
2023-01-09 17:36:18 +0200

    Update AppStyled.js

--- src/components/App/AppStyled.js
+++ src/components/App/AppStyled.js
@@ -1,29 +1,35 @@
 import styled from '@emotion/styled';
 
-
 export const Container = styled.div`
-display: flex;
-flex-direction: column;
-padding: 30px;
-font-family: 'Philosopher', sans-serif;
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  font-family: 'Philosopher', sans-serif;
+  @media screen and (max-width: 767px) {
 
-`
+padding: 15px;
+}
+`;
 
 export const Title = styled.h1`
-
-text-align: center;
-margin-bottom: 50px;
-font-family: 'Philosopher', sans-serif;
-font-weight: 700;
-`
+  text-align: center;
+  margin-bottom: 50px;
+  font-family: 'Philosopher', sans-serif;
+  font-weight: 700;
+`;
 
 export const Wrap = styled.div`
-width: 630px;
-display: inline-flex;
-flex-direction: column;
-padding: 20px;
-border: 2px solid green;
-border-radius: 20px;
-margin-right: auto;
-margin-left: auto;
-`
+  width: 630px;
+  display: inline-flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 2px solid green;
+  border-radius: 20px;
+  margin-right: auto;
+  margin-left: auto;
+
+  @media screen and (max-width: 767px) {
+    padding: 10px;
+    width: 260px;
+  }
+`;
